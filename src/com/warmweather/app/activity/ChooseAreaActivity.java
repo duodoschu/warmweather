795db81d2fec1b5a68b1e08eff5f4ccdcd28e3d9
yqihui <yqihui@gmail.com>
2016-10-17 23:23:02 +0800

    加入显示天气信息的功能

--- src/com/warmweather/app/activity/ChooseAreaActivity.java
+++ src/com/warmweather/app/activity/ChooseAreaActivity.java
@@ -3,18 +3,12 @@ package com.warmweather.app.activity;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.warmweather.app.R;
-import com.warmweather.app.model.City;
-import com.warmweather.app.model.County;
-import com.warmweather.app.model.Province;
-import com.warmweather.app.model.WarmWeatherDB;
-import com.warmweather.app.util.HttpCallbackListener;
-import com.warmweather.app.util.HttpUtil;
-import com.warmweather.app.util.Utility;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.text.TextUtils;
 import android.view.View;
 import android.view.Window;
@@ -24,6 +18,15 @@ import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.warmweather.app.R;
+import com.warmweather.app.model.City;
+import com.warmweather.app.model.County;
+import com.warmweather.app.model.Province;
+import com.warmweather.app.model.WarmWeatherDB;
+import com.warmweather.app.util.HttpCallbackListener;
+import com.warmweather.app.util.HttpUtil;
+import com.warmweather.app.util.Utility;
 
 public class ChooseAreaActivity extends Activity {
 	public static final int LEVEL_PROVINCE = 0;
@@ -51,6 +54,15 @@ public class ChooseAreaActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState){
 		super.onCreate(savedInstanceState);
+		
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
+		if(prefs.getBoolean("city_selected", false)){
+			Intent intent = new Intent(this,WeatherActivity.class);
+			startActivity(intent);
+			finish();
+			return;
+		}
+		
 		requestWindowFeature(Window.FEATURE_NO_TITLE);
 		setContentView(R.layout.choose_area);
 		listView = (ListView)findViewById(R.id.list_view);
@@ -67,6 +79,12 @@ public class ChooseAreaActivity extends Activity {
 				}else if(currentLevel == LEVEL_CITY){
 					selectedCity = cityList.get(index);
 					queryCounties();
+				}else if(currentLevel==LEVEL_COUNTY){
+					String countyCode = countyList.get(index).getCountyCode();
+					Intent intent = new Intent(ChooseAreaActivity.this,WeatherActivity.class);
+					intent.putExtra("county_code", countyCode);
+					startActivity(intent);
+					finish();
 				}
 			}
 		});
